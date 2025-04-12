46ec20d57f478a9d7eeb4bba8728ed07468fa1ba
Dev-barbosajv <joaovitorbarbosa513@gmail.com>
2025-04-12 10:54:30 -0300

    variaveis e tipos

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Joao Vitor"
+	var numeroAgencia int = 589
+	var numeroConta int = 12345
+	var saldo float32 = 125.50
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo) 
+}
+	
